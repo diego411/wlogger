87f7f18a5097ad0f6997b6e828a3f93d6ae52766
diego411 <diego.enrico@directbox.com>
2022-05-29 19:44:46 +0200

    update config url

--- src/controllers/config.rs
+++ src/controllers/config.rs
@@ -9,7 +9,7 @@ pub async fn fetch_config() -> Result<ConfigResponse, Box<dyn std::error::Error>
     let client = reqwest::Client::new();
     let config_service_url = env::var("CONFIG_SERVER_URL").expect("CONFIG_SERVER_URL must be set");
     let resp = client
-        .get(config_service_url + "api/v1/channels")
+        .get(config_service_url + "api/v1/config")
         .send()
         .await?;
     let resp_body = resp.text().await?;
